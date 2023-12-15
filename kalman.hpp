da7e5f0d7a9d7bac4a319826bf5b1904ec97ffc6
DiToMaVe <32896676+DiToMaVe@users.noreply.github.com>
2023-12-15 16:33:54 +0100

    Update kalman.hpp

--- kalman.hpp
+++ kalman.hpp
@@ -7,49 +7,83 @@ using Eigen::VectorXd;
 
 #pragma once
 
-class Measurements{
+class Measurements {
 
-    public:
-    const Eigen::MatrixXd U, Y;
+	/*
+	Z: the data structure containing the measured outputs in Z.y and 
+	possibly the measured inputs in Z.u. If the number of data
+	points is N, the number of outputs is p, and the number of
+	inputs is m, then Z.y is an N x p matrix and Z.u is an N x m
+	matrix.*/
+	// ToDo: Assess final choice N x ... or ... x N
 
-    Measurements(const Eigen::MatrixXd U, const Eigen::MatrixXd Y);
+	public:
+		const Eigen::MatrixXd U, Y;
+
+		Measurements(const Eigen::MatrixXd U, const Eigen::MatrixXd Y);
 
 };
 
-class LtiModel{
-    
-    public:
+class LssModel {
 
-    int n, m, p;
-    const VectorXd x0;
-    const MatrixXd A, B, C, D, P, Q, R, S, P0;
-    
-    LtiModel(
-        const MatrixXd A,
-        const MatrixXd B, 
-        const MatrixXd C, 
-        const MatrixXd D,
-        const MatrixXd P,
-        const MatrixXd Q,
-        const MatrixXd R,
-        const MatrixXd S,
-        const VectorXd x0,
-        const MatrixXd P0
-    );
+	/*	
+	Linear state space (ss) model, currently only a linear time-invariant (LTI) state space model is implemented.
+
+	For LssModel M()
+	M.A,B,C,D: system matrices; for each matrix.
+	M.Q,S,R: covariance matrices for process and measurement noise, respectively. 
+	M.x0,P0: Initial state mean (X1) and its covariance matrix (P1), respectively.
+
+	Dimensions:
+
+	n: number of states
+	p: number of inputs
+	m: number of outputs
+	
+	A(n, n)
+	B(n, p)
+	C(n, m)
+	D(m, p)
+	Q(n, n)
+	S(m, n)
+	R(m, m)
+	x1(n, 1)
+	P1(n, n)
+
+	*/
+
+	public:
+
+		int n, p, m;
+		const VectorXd x0;
+		const MatrixXd A, B, C, D, P, Q, R, S, P0;
+
+		LssModel(
+			const MatrixXd A,
+			const MatrixXd B,
+			const MatrixXd C,
+			const MatrixXd D,
+			const MatrixXd P,
+			const MatrixXd Q,
+			const MatrixXd R,
+			const MatrixXd S,
+			const VectorXd x0,
+			const MatrixXd P0
+		);
 };
 
-class Kalman{
+class Kalman {
 
-    public:
-        const Measurements& Z;
-        const LtiModel& M;
+	public:
+		const Measurements& Z;
+		const LssModel& M;
 
-        const MatrixXd I_n, I_m;
-        const MatrixXd R_inv, A_bar, B_bar, Q_bar, Q_root, R_root;
+		const MatrixXd I_n, I_m;
+		const MatrixXd R_inv, A_bar, B_bar, Q_bar, Q_root, R_root;
 
-        Kalman(Measurements &Z, LtiModel &M);
+		Kalman(Measurements &Z, LssModel &M);
 
-        std::tuple<VectorXd, VectorXd, MatrixXd> kalmanUpdate(VectorXd &xf, MatrixXd &Pf_root, VectorXd &u, VectorXd &y);
+		std::tuple<VectorXd, VectorXd, MatrixXd> kalmanUpdate(VectorXd &xf, MatrixXd &Pf_root, VectorXd &u, VectorXd &y);
 
-        static std::tuple<std::vector<VectorXd>, std::vector<VectorXd>, std::vector<MatrixXd>> kf(Measurements &Z, LtiModel &M);
-};
+		static std::tuple<std::vector<VectorXd>, std::vector<VectorXd>, std::vector<MatrixXd>> kf(Measurements &Z, LssModel &M);
+}; 
